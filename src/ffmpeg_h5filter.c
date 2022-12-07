a9143a51dcda9ae030fd3a8a607973467e09296d
Bin Duan <31501302+tuffr5@users.noreply.github.com>
2022-12-07 10:35:46 -0500

    Update ffmpeg_h5filter.c

--- src/ffmpeg_h5filter.c
+++ src/ffmpeg_h5filter.c
@@ -186,7 +186,7 @@ static void find_decoder_name(int c_id, char *name)
 static void encode(AVCodecContext *enc_ctx, AVFrame *frame, AVPacket *pkt, size_t *out_size, uint8_t **out_data)
 {
     int ret;
-        size_t offset = 0;
+    size_t offset = 0;
 
     /* send the frame to the encoder */
     if (frame)
