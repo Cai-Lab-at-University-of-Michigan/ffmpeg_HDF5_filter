5e43725298594d7ed8a2896a788b21582faf5273
tuffr5 <1527028326@qq.com>
2022-12-09 11:24:29 -0500

    initial testing python code

---
+++ python/example.py
@@ -0,0 +1,115 @@
+""" This example runs a simple compression test on raw 3D tif
+
+    Author: Bin Duan (bduan2@hawk.iit.edu)
+    Created: 2022
+
+"""
+
+import json
+import os
+import time
+
+import h5py
+import hdf5plugin as h5pl
+import numpy as np
+from tifffile import imread, imwrite
+from utils.metric_utils import BenchmarkMeter
+
+TEMPLATE = {
+    'enc_elapsed_time': '',
+    'dec_elapsed_time': '',
+    'cpu_peak_memory': '',
+    'gpu_peak_memory': '',
+    'cs_ratio': '',
+    'rmse': '',
+    'psnr': '',
+    'ssim': '',
+}
+
+
+def rgb_main(raw_file, h5_file, rec_file):
+    raw_array = imread(raw_file)  # ZCYX
+    raw_array = np.transpose(raw_array, (0, 2, 3, 1))  # ZYXC
+    print(f'RAW SHAPE: {raw_array.shape}')
+
+    chunk_shape = raw_array.shape
+    
+    print(f'CHUNK SHAPE: {chunk_shape}')
+    filter_parameters = (1, 1, raw_array.shape[2],
+                         raw_array.shape[1], raw_array.shape[0], 0)
+
+    record = TEMPLATE
+
+    # compressed to h5 file
+    start_time = time.time()
+    with h5py.File(hdf5_file, 'w') as f:
+        f.create_dataset('data', raw_array.shape, raw_array.dtype, data=raw_array,
+                         chunks=chunk_shape, **h5pl.FFMPEG(*filter_parameters))
+
+    record['enc_elapsed_time'] = f'{time.time() - start_time:.4f} s'
+
+    # decompressed back and store it
+    start_time = time.time()
+    with h5py.File(hdf5_file, 'r') as f:
+        rec_array = np.array(f['data'])
+
+    record['dec_elapsed_time'] = f'{time.time() - start_time:.4f} s'
+
+    print(f'REC SHAPE: {rec_array.shape}')
+
+    assert raw_array.shape == rec_array.shape
+
+    imwrite(rec_file, rec_array)
+
+    # metrics
+    record = benchmark_meter(record, raw_file, h5_file)
+
+    print(json.dumps(record, indent=2))
+
+
+def gray_main(raw_file, h5_file, rec_file):
+    raw_array = imread(raw_file)  # ZYX
+    print(f'RAW SHAPE: {raw_array.shape}')
+
+    chunk_shape = (832, 1024, 1024)
+    
+    print(f'CHUNK SHAPE: {chunk_shape}')
+    filter_parameters = (1, 1, raw_array.shape[2],
+                         raw_array.shape[1], raw_array.shape[0], 0)
+
+    record = TEMPLATE
+
+    # compressed to h5 file
+    start_time = time.time()
+    with h5py.File(hdf5_file, 'w') as f:
+        f.create_dataset('data', raw_array.shape, raw_array.dtype, data=raw_array,
+                         chunks=chunk_shape, **h5pl.FFMPEG(*filter_parameters))
+
+    record['enc_elapsed_time'] = f'{time.time() - start_time:.4f} s'
+
+    # decompressed back and store it
+    start_time = time.time()
+    with h5py.File(hdf5_file, 'r') as f:
+        rec_array = np.array(f['data'])
+
+    record['dec_elapsed_time'] = f'{time.time() - start_time:.4f} s'
+
+    print(f'REC SHAPE: {rec_array.shape}')
+
+    assert raw_array.shape == rec_array.shape
+
+    imwrite(rec_file, rec_array)
+
+    # metrics
+    record = benchmark_meter(record, raw_file, h5_file)
+
+    print(json.dumps(record, indent=2))
+
+
+if __name__ == '__main__':
+    tif_file = '/home/binduan/Downloads/182725_16X.tif'
+    hdf5_file = '/home/binduan/Downloads/182725_16X.h5'
+    rec_tif_file = '/home/binduan/Downloads/Rec_182725_16X.tif'
+    benchmark_meter = BenchmarkMeter()
+
+    gray_main(tif_file, hdf5_file, rec_tif_file)
---
+++ python/utils/metric_utils.py
@@ -0,0 +1,177 @@
+# evaluation relation utils
+import glob
+import os
+import shutil
+import subprocess
+
+import cv2
+import numpy as np
+from PIL import Image
+from tifffile import imread
+import h5py
+from skimage.metrics import normalized_root_mse, peak_signal_noise_ratio, structural_similarity
+from skimage import img_as_float
+
+
+
+
+## compression related
+## video file characteristics
+### file size
+def get_size_in_bit(filestr):
+    file_size = os.path.getsize(filestr)
+
+    return file_size * 8
+
+
+def get_size_in_byte(filestr):
+    file_size = os.path.getsize(filestr)
+
+    return file_size
+
+
+def get_size_in_KB(filestr):
+    file_size = get_size_in_byte(filestr)
+
+    return file_size / 1024
+
+
+def get_size_in_MB(filestr):
+    file_size = get_size_in_KB(filestr)
+
+    return file_size / 1024
+
+
+def get_size_in_GB(filestr):
+    file_size = get_size_in_MB(filestr)
+
+    return file_size / 1024
+
+
+def byte_to_bit(byte_):
+    return byte_ * 8
+
+
+def bit_to_byte(bit_):
+    return bit_ / 8
+
+
+### compressed ratio
+def get_compression_ratio(src_file, comp_file):
+    return get_size_in_byte(src_file) / get_size_in_byte(comp_file)
+
+
+### fps
+# def get_frame_rate_ffmpeg(filename):
+#     if not os.path.exists(filename):
+#         sys.stderr.write("ERROR: filename %r was not found!" % (filename,))
+#         return -1
+#     out = subprocess.check_output(
+#         ["ffprobe", filename, "-v", "0", "-select_streams", "v", "-print_format", "flat", "-show_entries",
+#          "stream=r_frame_rate"])
+#     rate = out.split('=')[1].strip()[1:-1].split('/')
+#     if len(rate) == 1:
+#         return float(rate[0])
+#     if len(rate) == 2:
+#         return float(rate[0]) / float(rate[1])
+#     return -1
+
+
+def get_frame_rate(filestr):
+    cap = cv2.VideoCapture(filestr)
+
+    fps = cap.get(cv2.CV_CAP_PROP_FPS)
+
+    cap.release()
+
+    return fps
+
+
+### bitrate
+# def get_duration_ffmpeg(filename):
+#     if not os.path.exists(filename):
+#         sys.stderr.write("ERROR: filename %r was not found!" % (filename,))
+#         return -1
+#     out = subprocess.check_output(
+#         ["ffprobe", "-i", filename, "-show_entries", "format=duration", "-v", "quiet", "-of", "csv=p=0"])
+#     assert len(out) == 1
+#     return float(out[0])
+
+
+def get_duration(filestr):
+    cap = cv2.VideoCapture(filestr)
+
+    fps = cap.get(cv2.CAP_PROP_FPS)
+    length = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+    print(f'filestr: {filestr}, fps: {fps}, length: {length}')
+
+    cap.release()
+
+    return length / fps
+
+
+def get_bitrate(filestr):
+    file_size = get_size_in_bit(filestr)
+    duration = get_duration(filestr)
+
+    return file_size / 1024 / duration
+
+
+# images
+def get_all_frames(filestr, dir_str, flag):
+    cmd = f'ffmpeg -i {filestr} {dir_str}/{flag}-%6d.png'
+    # cmd = 'export LD_LIBRARY_PATH="/home/binduan/ffmpeg_build/lib/:$LD_LIBRARY_PATH" && ' + cmd
+
+    process = subprocess.Popen(cmd, shell=True)
+    process.wait()
+
+    imgs = sorted(glob.glob(f'{dir_str}/{flag}-*.png'))
+    return imgs
+
+
+def tifread(filestr):
+    img = imread(filestr) # ZCYX
+    if img.ndim == 4:
+        img = np.transpose(img, (0, 2, 3, 1)) # ZYXC
+
+    return img
+
+
+def h5read(filestr):
+    with h5py.File(filestr, 'r') as f:
+        data = np.array(f['data'])
+    
+    return data
+
+
+class BenchmarkMeter(object):
+    """docstring for BenchmarkMeter"""
+
+    def __init__(self):
+        super(BenchmarkMeter, self).__init__()
+
+    def __call__(self, record_template: dict, src_file: str, cs_file: str) -> dict:
+        record_template['cs_ratio'] = f'{get_compression_ratio(src_file, cs_file):.4f}'
+
+        src_imgs = tifread(src_file) # ZYXC
+        cs_imgs = h5read(cs_file) # ZYXC
+
+        assert len(src_imgs) == len(cs_imgs), print(f'src_file: {len(src_imgs)}, cs_file: {len(cs_imgs),}')
+
+        rmse = []
+        psnr = []
+        ssim = []
+
+        for (src_im, cs_im) in zip(src_imgs, cs_imgs):
+            src_im = np.array(img_as_float(src_im))
+            cs_im = np.array(img_as_float(cs_im))
+
+            rmse.append(normalized_root_mse(src_im, cs_im))
+            psnr.append(peak_signal_noise_ratio(src_im, cs_im))
+            ssim.append(structural_similarity(src_im, cs_im, channel_axis=-1))
+
+        record_template['rmse'] = f'{np.mean(np.array(rmse)):.4f}'
+        record_template['psnr'] = f'{np.mean(np.array(psnr)):.4f}'
+        record_template['ssim'] = f'{np.mean(np.array(ssim)):.4f}'
+
+        return record_template
