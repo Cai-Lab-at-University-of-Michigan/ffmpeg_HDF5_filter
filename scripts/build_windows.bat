025f3664e796072b57714a188ff9718a12aba9c5
tuffr5 <tuffrr5@gmail.com>
2025-05-28 02:07:55 +0000

    fix unzip of hdf5

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -1,16 +1,18 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Set paths
-set HOME=D:\a
-set FFMPEG_ROOT=%HOME%\ffmpeg_build
+:: ----------------------------------------------------------------------------
+:: Set up directories
+set "HOME=D:\a"
+set "FFMPEG_ROOT=%HOME%\ffmpeg_build"
+set "TEMP_BUILD=%HOME%\temp_build"
 
 :: Create required directories
-for %%D in (%FFMPEG_ROOT% %FFMPEG_ROOT%\bin %FFMPEG_ROOT%\lib %FFMPEG_ROOT%\include %HOME%\temp_build) do (
-    if not exist "%%D" mkdir "%%D"
+for %%D in ("%FFMPEG_ROOT%" "%FFMPEG_ROOT%\bin" "%FFMPEG_ROOT%\lib" "%FFMPEG_ROOT%\include" "%TEMP_BUILD%") do (
+    if not exist %%D mkdir %%D
 )
 
-cd /D %HOME%\temp_build
+cd /D "%TEMP_BUILD%"
 
 :: ----------------------------------------------------------------------------
 :: Download and extract HDF5 (MSVC-compatible)
@@ -19,21 +21,25 @@ curl -L --retry 3 --retry-delay 5 ^
   https://github.com/HDFGroup/hdf5/releases/download/hdf5_1.14.6/hdf5-1.14.6-win-vs2022_cl.zip ^
   -o hdf5-msvc.zip
 
-:: First unzip: outer archive
 7z x hdf5-msvc.zip -ohdf5_outer
 
-:: Cd hdf5_outer
-cd /D hdf5_outer
+pushd hdf5_outer
 
-:: Second unzip: nested HDF5 zip file
-7z x HDF5-1.14.6-win64.zip -ohdf5_msvc
-
-:: Copy headers, libs, DLLs
-for /D %%D in (hdf5_msvc\HDF5-*-win64) do (
-    xcopy "%%D\include\*" %FFMPEG_ROOT%\include\ /E /I /Q
-    xcopy "%%D\lib\*" %FFMPEG_ROOT%\lib\ /E /I /Q
-    xcopy "%%D\bin\*.dll" %FFMPEG_ROOT%\bin\ /E /I /Q
+:: Extract nested zip (wildcard in case of version variation)
+for %%F in (*.zip) do (
+    7z x "%%F" -ohdf5_msvc
+    goto :hdf5_unzipped
 )
+:hdf5_unzipped
+
+for /D %%D in (hdf5_msvc\HDF5-*-win64) do (
+    echo Copying HDF5 files from %%D...
+    xcopy /E /I /Q /Y "%%D\include\*" "%FFMPEG_ROOT%\include\"
+    xcopy /E /I /Q /Y "%%D\lib\*" "%FFMPEG_ROOT%\lib\"
+    xcopy /E /I /Q /Y "%%D\bin\*.dll" "%FFMPEG_ROOT%\bin\"
+)
+
+popd
 
 :: ----------------------------------------------------------------------------
 :: Download and extract FFmpeg (MSVC-compatible)
@@ -45,14 +51,16 @@ curl -L --retry 3 --retry-delay 5 ^
 7z x ffmpeg-msvc.zip -offmpeg_msvc
 
 for /D %%D in (ffmpeg_msvc\ffmpeg-*-full_build-shared) do (
-    xcopy "%%D\include\*" %FFMPEG_ROOT%\include\ /E /I /Q
-    xcopy "%%D\lib\*" %FFMPEG_ROOT%\lib\ /E /I /Q
-    xcopy "%%D\bin\*.dll" %FFMPEG_ROOT%\bin\ /E /I /Q
+    echo Copying FFmpeg files from %%D...
+    xcopy /E /I /Q /Y "%%D\include\*" "%FFMPEG_ROOT%\include\"
+    xcopy /E /I /Q /Y "%%D\lib\*" "%FFMPEG_ROOT%\lib\"
+    xcopy /E /I /Q /Y "%%D\bin\*.dll" "%FFMPEG_ROOT%\bin\"
 )
 
 :: ----------------------------------------------------------------------------
+:: Done
 echo.
 echo ✅ Prebuilt library setup complete!
 echo 🔧 Installed to: %FFMPEG_ROOT%
 echo.
-dir /S /B %FFMPEG_ROOT%
+dir /S /B "%FFMPEG_ROOT%"
