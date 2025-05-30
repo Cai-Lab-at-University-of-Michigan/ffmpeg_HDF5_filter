91c289de011998cb924578bbeca5f14e39895a64
tuffr5 <tuffrr5@gmail.com>
2025-05-30 21:11:16 +0000

    update jar name

--- setup.py
+++ setup.py
@@ -421,7 +421,7 @@ except FileNotFoundError:
 
 setup(
     name="h5ffmpeg",
-    version="1.8.2",
+    version="1.8.4",
     description="HDF5 filter plugin for FFMPEG video codec compression",
     long_description=long_description,
     long_description_content_type="text/markdown",
