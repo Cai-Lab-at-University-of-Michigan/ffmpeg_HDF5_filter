a6ef0b1b16c18af72ad934ddabe3211f369be0cc
tuffr5 <tuffrr5@gmail.com>
2025-08-11 15:10:22 +0000

    fix macos libs link

--- cmake/macos.cmake
+++ cmake/macos.cmake
@@ -77,6 +77,21 @@ else()
     message(WARNING "✗ HDF5 NOT FOUND")
 endif()
 
+find_library(ICONV_LIBRARY
+    NAMES iconv
+    PATHS 
+        ${HDF5_ROOT}/lib
+        ${FFMPEG_ROOT}/lib
+        /usr/local/lib
+        /opt/homebrew/lib
+)
+
+if(ICONV_LIBRARY)
+    message(STATUS "✓ libiconv: ${ICONV_LIBRARY}")
+else()
+    message(WARNING "libiconv not found - may cause runtime issues")
+endif()
+
 message(STATUS "=== SUMMARY ===")
 message(STATUS "FFMPEG_INCLUDE_DIR: ${FFMPEG_INCLUDE_DIR}")
 message(STATUS "HDF5_INCLUDE_DIR: ${HDF5_INCLUDE_DIR}")
@@ -123,6 +138,7 @@ target_link_libraries(h5ffmpeg_shared
     PRIVATE
         ${FFMPEG_LIBRARIES}
         ${HDF5_C_LIBRARY}
+        ${ICONV_LIBRARY}
         "-framework CoreFoundation"
         "-framework CoreMedia"
         "-framework CoreVideo"
@@ -150,91 +166,162 @@ install(FILES src/ffmpeg_utils.h
     DESTINATION include
 )
 
-if(EXISTS "${FFMPEG_ROOT}/lib")
-    file(GLOB FFMPEG_DYLIBS "${FFMPEG_ROOT}/lib/*.dylib")
-    if(FFMPEG_DYLIBS)
-        install(FILES ${FFMPEG_DYLIBS}
-            DESTINATION lib
-            OPTIONAL
-        )
-        message(STATUS "Will install FFmpeg dylibs: ${FFMPEG_DYLIBS}")
-    endif()
-endif()
-
-if(EXISTS "${HDF5_ROOT}/lib")
-    file(GLOB HDF5_DYLIBS "${HDF5_ROOT}/lib/*.dylib")
-    if(HDF5_DYLIBS)
-        install(FILES ${HDF5_DYLIBS}
-            DESTINATION lib
-            OPTIONAL
-        )
-        message(STATUS "Will install HDF5/conda dylibs: ${HDF5_DYLIBS}")
-    endif()
-endif()
+add_custom_target(bundle_all ALL
+    DEPENDS h5ffmpeg_shared
+    COMMENT "Bundling all dependencies for standalone distribution"
+)
 
 install(CODE "
     set(MAIN_LIB \"\${CMAKE_INSTALL_PREFIX}/lib/libh5ffmpeg_shared.dylib\")
     
     if(EXISTS \"\${MAIN_LIB}\")
+        set(FFMPEG_ROOT \"${FFMPEG_ROOT}\")
+        set(HDF5_ROOT \"${HDF5_ROOT}\")
+        
+        set(target_libs
+            \"libavcodec\" \"libavformat\" \"libavutil\" \"libswscale\" \"libswresample\" \"libavfilter\"
+            \"libhdf5\"
+            \"libx264\" \"libx265\" \"libvpx\" \"libdav1d\" \"libaom\" \"librav1e\" \"libSvtAv1Enc\"
+            \"libiconv\"
+        )
+        
+        file(GLOB_RECURSE candidate_files 
+            \"\${FFMPEG_ROOT}/lib/*.dylib\"
+            \"\${HDF5_ROOT}/lib/*.dylib\"
+        )
+        
+        foreach(candidate_file \${candidate_files})
+            get_filename_component(candidate_name \"\${candidate_file}\" NAME)
+            
+            set(should_bundle FALSE)
+            foreach(target_lib \${target_libs})
+                if(\"\${candidate_name}\" MATCHES \"^\${target_lib}[.].*[.]dylib$\" OR 
+                   \"\${candidate_name}\" MATCHES \"^\${target_lib}[.]dylib$\")
+                    set(should_bundle TRUE)
+                    break()
+                endif()
+            endforeach()
+            
+            if(should_bundle)
+                if(EXISTS \"\${candidate_file}\" AND NOT IS_SYMLINK \"\${candidate_file}\")
+                    set(dest_file \"\${CMAKE_INSTALL_PREFIX}/lib/\${candidate_name}\")
+                    if(NOT EXISTS \"\${dest_file}\")
+                        file(COPY \"\${candidate_file}\" DESTINATION \"\${CMAKE_INSTALL_PREFIX}/lib\")
+                        execute_process(COMMAND \${CMAKE_COMMAND} -E echo \"Bundled: \${candidate_name}\")
+                    endif()
+                elseif(IS_SYMLINK \"\${candidate_file}\")
+                    execute_process(COMMAND readlink -f \"\${candidate_file}\" OUTPUT_VARIABLE real_file OUTPUT_STRIP_TRAILING_WHITESPACE ERROR_QUIET)
+                    if(EXISTS \"\${real_file}\")
+                        get_filename_component(real_name \"\${real_file}\" NAME)
+                        set(dest_real_file \"\${CMAKE_INSTALL_PREFIX}/lib/\${real_name}\")
+                        
+                        if(NOT EXISTS \"\${dest_real_file}\")
+                            file(COPY \"\${real_file}\" DESTINATION \"\${CMAKE_INSTALL_PREFIX}/lib\")
+                        endif()
+                        
+                        set(dest_symlink \"\${CMAKE_INSTALL_PREFIX}/lib/\${candidate_name}\")
+                        if(NOT EXISTS \"\${dest_symlink}\")
+                            execute_process(COMMAND ln -sf \"\${real_name}\" \"\${dest_symlink}\" ERROR_QUIET)
+                        endif()
+                        execute_process(COMMAND \${CMAKE_COMMAND} -E echo \"Bundled symlink: \${candidate_name} -> \${real_name}\")
+                    endif()
+                endif()
+            endif()
+        endforeach()
+        
         find_program(INSTALL_NAME_TOOL install_name_tool)
         if(INSTALL_NAME_TOOL)
-            file(GLOB ALL_DYLIBS \"\${CMAKE_INSTALL_PREFIX}/lib/*.dylib\")
+            file(GLOB bundle_libs \"\${CMAKE_INSTALL_PREFIX}/lib/*.dylib\")
             
-            foreach(current_dylib \${ALL_DYLIBS})
-                get_filename_component(current_name \"\${current_dylib}\" NAME)
-                
-                execute_process(
-                    COMMAND \"\${INSTALL_NAME_TOOL}\" -id \"@loader_path/\${current_name}\" \"\${current_dylib}\"
-                    ERROR_QUIET
-                )
-                
-                execute_process(
-                    COMMAND otool -L \"\${current_dylib}\"
-                    OUTPUT_VARIABLE deps
-                    ERROR_QUIET
-                )
-                
-                foreach(other_dylib \${ALL_DYLIBS})
-                    get_filename_component(other_name \"\${other_dylib}\" NAME)
+            foreach(lib \${bundle_libs})
+                if(NOT IS_SYMLINK \"\${lib}\")
+                    get_filename_component(lib_name \"\${lib}\" NAME)
                     
-                    string(FIND \"\${deps}\" \"\${other_name}\" found_dep)
-                    if(NOT found_dep EQUAL -1)
-                        # Fix absolute paths to @loader_path
-                        execute_process(
-                            COMMAND \"\${INSTALL_NAME_TOOL}\" -change 
-                                \"/Users/runner/ffmpeg/lib/\${other_name}\"
-                                \"@loader_path/\${other_name}\"
-                                \"\${current_dylib}\"
-                            ERROR_QUIET
-                        )
-                        execute_process(
-                            COMMAND \"\${INSTALL_NAME_TOOL}\" -change 
-                                \"/Users/runner/miniconda/lib/\${other_name}\"
-                                \"@loader_path/\${other_name}\"
-                                \"\${current_dylib}\"
-                            ERROR_QUIET
-                        )
-                        execute_process(
-                            COMMAND \"\${INSTALL_NAME_TOOL}\" -change 
-                                \"@rpath/\${other_name}\"
-                                \"@loader_path/\${other_name}\"
-                                \"\${current_dylib}\"
-                            ERROR_QUIET
-                        )
-                    endif()
-                endforeach()
+                    execute_process(
+                        COMMAND \"\${INSTALL_NAME_TOOL}\" -id \"@loader_path/\${lib_name}\" \"\${lib}\"
+                        ERROR_QUIET
+                    )
+                    
+                    execute_process(
+                        COMMAND otool -L \"\${lib}\"
+                        OUTPUT_VARIABLE deps
+                        ERROR_QUIET
+                    )
+                    
+                    foreach(other_lib \${bundle_libs})
+                        get_filename_component(other_name \"\${other_lib}\" NAME)
+                        
+                        string(FIND \"\${deps}\" \"\${other_name}\" found_dep)
+                        if(NOT found_dep EQUAL -1)
+                            execute_process(
+                                COMMAND \"\${INSTALL_NAME_TOOL}\" -change 
+                                    \"\${FFMPEG_ROOT}/lib/\${other_name}\"
+                                    \"@loader_path/\${other_name}\"
+                                    \"\${lib}\"
+                                ERROR_QUIET
+                            )
+                            execute_process(
+                                COMMAND \"\${INSTALL_NAME_TOOL}\" -change 
+                                    \"\${HDF5_ROOT}/lib/\${other_name}\"
+                                    \"@loader_path/\${other_name}\"
+                                    \"\${lib}\"
+                                ERROR_QUIET
+                            )
+                            execute_process(
+                                COMMAND \"\${INSTALL_NAME_TOOL}\" -change 
+                                    \"@rpath/\${other_name}\"
+                                    \"@loader_path/\${other_name}\"
+                                    \"\${lib}\"
+                                ERROR_QUIET
+                            )
+                            execute_process(
+                                COMMAND \"\${INSTALL_NAME_TOOL}\" -change 
+                                    \"/usr/local/lib/\${other_name}\"
+                                    \"@loader_path/\${other_name}\"
+                                    \"\${lib}\"
+                                ERROR_QUIET
+                            )
+                            execute_process(
+                                COMMAND \"\${INSTALL_NAME_TOOL}\" -change 
+                                    \"/opt/homebrew/lib/\${other_name}\"
+                                    \"@loader_path/\${other_name}\"
+                                    \"\${lib}\"
+                                ERROR_QUIET
+                            )
+                        endif()
+                    endforeach()
+                endif()
             endforeach()
             
             execute_process(COMMAND otool -L \"\${MAIN_LIB}\" OUTPUT_VARIABLE DEPS_AFTER ERROR_QUIET)
-            execute_process(COMMAND \${CMAKE_COMMAND} -E echo \"Main library dependencies:\")
+            execute_process(COMMAND \${CMAKE_COMMAND} -E echo \"=== Main library dependencies after bundling ===\")
             execute_process(COMMAND \${CMAKE_COMMAND} -E echo \"\${DEPS_AFTER}\")
             
             string(REGEX MATCH \"/Users/[^/]*/[^\\n]*\" found_absolute \"\${DEPS_AFTER}\")
-            if(found_absolute)
-                execute_process(COMMAND \${CMAKE_COMMAND} -E echo \"WARNING: Absolute paths still found\")
+            string(REGEX MATCH \"/usr/local/[^\\n]*\" found_usr_local \"\${DEPS_AFTER}\")
+            string(REGEX MATCH \"/opt/homebrew/[^\\n]*\" found_homebrew \"\${DEPS_AFTER}\")
+            
+            if(found_absolute OR found_usr_local OR found_homebrew)
+                execute_process(COMMAND \${CMAKE_COMMAND} -E echo \"WARNING: Some absolute paths still found in dependencies\")
+                if(found_absolute)
+                    execute_process(COMMAND \${CMAKE_COMMAND} -E echo \"  User paths: \${found_absolute}\")
+                endif()
+                if(found_usr_local)
+                    execute_process(COMMAND \${CMAKE_COMMAND} -E echo \"  /usr/local paths: \${found_usr_local}\")
+                endif()
+                if(found_homebrew)
+                    execute_process(COMMAND \${CMAKE_COMMAND} -E echo \"  Homebrew paths: \${found_homebrew}\")
+                endif()
             else()
                 execute_process(COMMAND \${CMAKE_COMMAND} -E echo \"SUCCESS: All non-system dependencies use @loader_path\")
+                execute_process(COMMAND \${CMAKE_COMMAND} -E echo \"System frameworks (CoreFoundation, etc.) will be dynamically resolved at runtime\")
             endif()
+
+            execute_process(COMMAND \${CMAKE_COMMAND} -E echo \"=== Bundled libraries ===\")
+            foreach(bundled_lib \${bundle_libs})
+                get_filename_component(bundled_name \"\${bundled_lib}\" NAME)
+                execute_process(COMMAND \${CMAKE_COMMAND} -E echo \"  \${bundled_name}\")
+            endforeach()
         endif()
     endif()
 ")
